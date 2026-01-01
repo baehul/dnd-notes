0e3591fbb5e2815ccbcb3336972212ba02a77153
Mehul PC <mgupta320@gatech.edu>
2026-01-01 02:10:02 -0500

    Quartz sync: Jan 1, 2026, 2:10 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -48,7 +48,11 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Explorer(),
   ],
   right: [
-    Component.Graph(),
+    Component.Graph({
+      globalGraph: {
+        showTags: false,
+      },
+    }),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
